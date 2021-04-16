c57b48134eca030ae4d0b8c22a4759d2e66a8d2f
Thomas Gull <thomas@gulldesign.ch>
2021-04-16 17:04:54 +0200

    update, rearange and finishing up.

--- index.php
+++ index.php
@@ -2,143 +2,239 @@
 $pageTitle = "Overview";
 include_once '../_template/head.php';
 ?>
+
 <body id="helpportal">
-<div class="container">
-	<div class="row justify-content-center">
-		<div class="col-sm-12 col-md-12">
-			<?php include_once '../_template/header.php'; ?>
-			<section class="mainContent">
-				<div class="container">
-					<div class="row mb-4">
-						<div class="col-sm-12 col-md-12 mb-12">
-							<h1>Helpportal</h1>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-sm-12 col-md-12">
+				<?php include_once '../_template/header.php'; ?>
+				<section class="mainContent">
+					<div class="container">
+						<div class="row mb-4">
+							<div class="col-sm-12 col-md-12 mb-12">
+								<h1>Helpportal</h1>
+							</div>
+						</div>
+						<div class="row mb-5">
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../varcode/">
+										<div class="itemImage" style="background-image: url('img/pexels-alice-castro-1820041.jpg') ">
+											<div class="badge rounded-pill einkauf-color">
+												Einkauf
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../varcode/"><span class="dashboardTitle">Variationskarten</span></a>
+										<p class="dashboardDesc">Alle Variationskarten im geordneten Überblick. Wenn du eine neue Farbe oder Grösse benötigst, schau einfach hier nach den nötigen Positionen.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../deeplink/">
+										<div class="itemImage" style="background-image: url('img/pexels-johnmark-smith-250591.jpg') ">
+											<div class="badge rounded-pill marketing-color">
+												Marketing
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../deeplink/"><span class="dashboardTitle">Deeplink erstellen</span></a>
+										<p class="dashboardDesc">Ganz einfach einen Tracking-Link erstellen, welcher auf unserern Shop leitet und mit allen notwendigen Parametern oder Gutscheincode bestückt ist. Damit gelingt uns jede Auswertung.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../bulk-link-open/">
+										<div class="itemImage" style="background-image: url('img/pexels-alice-castro-1820041.jpg') ">
+											<div class="badge rounded-pill ecommerce-color">
+												eCommerce
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../bulk-link-open/"><span class="dashboardTitle">Multi-Artikel öffnen</span></a>
+										<p class="dashboardDesc">Eine Liste mit vielen Artikelnummern auf einen Schlag in unserem Webshop öffnen? Kein Problem.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../regex/">
+										<div class="itemImage" style="background-image: url('img/pexels-johnmark-smith-250591.jpg') ">
+											<div class="badge rounded-pill ecommerce-color">
+												eCommerce
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../regex/"><span class="dashboardTitle">Artikelnummern filtern</span></a>
+										<p class="dashboardDesc">Alle Arikelnummern aus einem Manus, einer Mail oder sonstigen Dokumenten rausfiltern, zählen lassen und als ordentliche Liste speichern, für die Weiterverarbeitung.</p>
+									</div>
+								</div>
+							</div>
+
+
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../taxonomy/">
+										<div class="itemImage" style="background-image: url('img/jungle.jpg') ">
+											<div class="badge rounded-pill ecommerce-color">
+												eCommerce
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../taxonomy/"><span class="dashboardTitle">Google Taxonomy</span></a>
+										<p class="dashboardDesc">Hier findest du sicher die notwendige Google Taxonomy, welche zu unseren hauseigenen Warengruppen korrespondiert und kannst sie dann sogleich im Shop oder ProductsUp einpflegen.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../scarab/">
+										<div class="itemImage" style="background-image: url('img/jungle.jpg') ">
+											<div class="badge rounded-pill ecommerce-color">
+												eCommerce
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../scarab/"><span class="dashboardTitle">Emarsys Reco Tester</span></a>
+										<p class="dashboardDesc">Wenn es mal schnell gehen muss, um die passenden Recos oder auch neue Warengruppen für NL Empfehlungen zu finden, dann bist du hier auf dem richtigen Weg. Es gibt sehr viele Möglichkeiten.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="../hash/">
+										<div class="itemImage" style="background-image: url('img/jungle.jpg') ">
+											<div class="badge rounded-pill ecommerce-color">
+												eCommerce
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="../hash/"><span class="dashboardTitle">E-Mail Hash erstellen</span></a>
+										<p class="dashboardDesc">Etwas technischer wird es beim hashen von E-Mails aus einer CSV-Datei. Dies dient zur Verschlüsselung, also für den Datenschutz.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem">
+									<a target="_blank" href="http://intern.angela.ch/onsigntv/birthday">
+										<div class="itemImage" style="background-image: url('img/birthday.jpg') ">
+											<div class="badge rounded-pill einkauf-color">
+												Allgemein
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="http://intern.angela.ch/onsigntv/birthday"><span class="dashboardTitle">Geburtstage</span></a>
+										<p class="dashboardDesc">Schau hier rein, wenn du wissen möchtest ob einer deiner Teamkamerad(in)en heute ihr Wiegenfest feiert. Happy Birthday! Alles Gute und viel Freude! </p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-sm-12 col-md-12 mb-2">
+								<h3>Ressources</h3>
+							</div>
+							<div class="col-sm-12 col-md-6">
+								<div class="dashboardItem">
+									<a target="_blank" href="https://www.gulldesign.ch/code/public">
+										<div class="itemImage" style="background-image: url('img/pexels-aleksandar-pasaric-1285625.jpg') ">
+											<div class="badge rounded-pill ressource-color">
+												Ressource
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="https://www.gulldesign.ch/code/public"><span class="dashboardTitle">miniPIM</span></a>
+										<p class="dashboardDesc">Artikelbriefing für den Shop erfassen. Suchen, Übersetzungen direkt einpflegen und alle nötigen Attribute erfassen. Am Ende kann dann ein automatisierter Export erstellt werden, welcher dem Webshop übergeben wird. Damit ist die Produktdatenerfassung für den Webshop der Angela Bruderer revolutioniert.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem ressource">
+									<a target="_blank" href="https://www.angela-bruderer.ch/abmageadm/">
+										<div class="itemImage" style="background-image: url('img/magentologo.png') ">
+											<div class="badge rounded-pill ressource-color">
+												Ressource
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="https://www.angela-bruderer.ch/abmageadm/"><span class="dashboardTitle">AB Magento Backend</span></a>
+										<p class="dashboardDesc">Shop Administration für den ganzen Angela Bruderer Webshop.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem ressource">
+									<a target="_blank" href="https://www.praktikus.ch/cs2/">
+										<div class="itemImage" style="background-image: url('img/magentologo.png') ">
+											<div class="badge rounded-pill ressource-color">
+												Ressource
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="https://www.praktikus.ch/cs2/"><span class="dashboardTitle">PRA Magento Backend</span></a>
+										<p class="dashboardDesc">Shop Administration für den ganzen Praktikus Webshop.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem ressource">
+									<a target="_blank" href="https://jira.dotsource.de/">
+										<div class="itemImage" style="background-image: url('img/jiralogo2.png') ">
+											<div class="badge rounded-pill ressource-color">
+												Ressource
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="https://jira.dotsource.de/"><span class="dashboardTitle">dot.Source Jira</span></a>
+										<p class="dashboardDesc">Ticketsystem für Aufgaben, Bugs und Projektmanagement mit der Webshop Agentur werden hiermit verwaltet.</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-12 col-md-3">
+								<div class="dashboardItem ressource">
+									<a target="_blank" href="https://jira.angela.ch/">
+										<div class="itemImage" style="background-image: url('img/jiralogo2.png') ">
+											<div class="badge rounded-pill ressource-color">
+												Ressource
+											</div>
+										</div>
+									</a>
+									<div class="itemBody">
+										<a target="_blank" href="https://jira.angela.ch/"><span class="dashboardTitle">AB Jira Intern</span></a>
+										<p class="dashboardDesc">Ticketsystem für Aufgaben, Bugs, Fehlermeldungen für den Shop werden hiermit verwaltet.</p>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="row mb-5">
-						<!-- <div class="col-sm-12 col-md-12 mb-2">
-							<h3>Marketing</h3>
-						</div> -->
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/pexels-johnmark-smith-250591.jpg') ">
-									<div class="badge rounded-pill ecommerce-color">
-										eCommerce
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="../regex/"><span class="dashboardTitle">Artikelnummern filtern</span></a>
-									<p class="dashboardDesc">Alle Arikelnummern aus einem Manus rausfiltern und als ordentliche Liste speichern.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/pexels-johnmark-smith-250591.jpg') ">
-									<div class="badge rounded-pill marketing-color">
-										Marketing
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="../deeplink/"><span class="dashboardTitle">Artikelnummern filtern</span></a>
-									<p class="dashboardDesc">Ganz einfach einen Tracking-Link erstellen, welcher auf unserern Shop leitet und mit allen notwendigen Parametern bestückt ist. Damit gelingt uns jede Auswertung.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/pexels-alice-castro-1820041.jpg') ">
-									<div class="badge rounded-pill ecommerce-color">
-										eCommerce
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="../bulk-link-open/"><span class="dashboardTitle">Bulk Link</span></a>
-									<p class="dashboardDesc">Viele Artikelnummern auf einmal im Webshop öffnen und kontrollieren lassen.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/jungle.jpg') ">
-									<div class="badge rounded-pill ecommerce-color">
-										eCommerce
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="../taxonomy/"><span class="dashboardTitle">Google Taxonomy</span></a>
-									<p class="dashboardDesc">Hier kannst du die passende Google Taxonomy Kategorie suchen und im Shop einpflegen.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/pexels-alice-castro-1820041.jpg') ">
-									<div class="badge rounded-pill einkauf-color">
-										Einkauf
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="../varcode/"><span class="dashboardTitle">Variationskarten</span></a>
-									<p class="dashboardDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum soluta nobis!</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-sm-12 col-md-12 mb-2">
-							<h3>Ressources</h3>
-						</div>
-						<div class="col-sm-12 col-md-6">
-							<div class="dashboardItem">
-								<div class="itemImage" style="background-image: url('img/pexels-aleksandar-pasaric-1285625.jpg') ">
-									<div class="badge rounded-pill ressource-color">
-										Ressource
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="https://www.gulldesign.ch/code/public"><span class="dashboardTitle">Artikelmanagement-Tool</span></a>
-									<p class="dashboardDesc">Artikelbriefing für den Shop erfassen. Übersetzungen und Exporte generieren.</p>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem ressource">
-								<div class="itemImage" style="background-image: url('img/jiralogo2.png') ">
-									<div class="badge rounded-pill ressource-color">
-										Ressource
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="http://jira.angela.ch/"><span class="dashboardTitle">Jira Ticketsystem</span></a>
-									<p class="dashboardDesc">Erstellen von Aufgaben oder Fehlermeldungen für den Shop.</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-12 col-md-3">
-							<div class="dashboardItem ressource">
-								<div class="itemImage" style="background-image: url('img/magentologo.png') ">
-									<div class="badge rounded-pill ressource-color">
-										Ressource
-									</div>
-								</div>
-								<div class="itemBody">
-									<a target="_blank" href="http://jira.dotsource.de/"><span class="dashboardTitle">Magento Backend</span></a>
-									<p class="dashboardDesc">Damit man gleichzeitig viele Links öffnen kann.</p>
-								</div>
-							</div>
-						</div>
-						
-					</div>
-				</div>
-			</section>
+				</section>
 				<!-- <div class="sideNav sticky-top">...</div> -->
+			</div>
 		</div>
 	</div>
-</div>
-<?php include '../_template/footer.php'; ?>
+	<?php include '../_template/footer.php'; ?>
